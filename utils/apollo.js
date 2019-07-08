d97f18c70e3182429e00d6c58ce1b7b62316bdde
MG2 Media <mg2@MG2-Media-Juliens-MacBook-Pro.local>
2019-07-08 12:55:26 -0400

    uploadLink apollo

--- utils/apollo.js
+++ utils/apollo.js
@@ -1,5 +1,11 @@
 import ApolloClient from "apollo-boost";
+import { createUploadLink } from 'apollo-upload-client';
+
+// Create a link that can upload blob file, mostly for image purpose
+const link = createUploadLink({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+})
 
 export const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io"
+  link
 });
